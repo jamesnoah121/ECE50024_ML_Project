003ed8a2e8904c212da8733615996f2a5728ef84
jamesnoah121 <52267017+jamesnoah121@users.noreply.github.com>
2023-04-28 15:50:58 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -3,13 +3,10 @@
 # ML Project - CGAN Reimplementation
 
 # Baseline ML Mastery CGAN by Jason Brownlee - FMNIST
-# Applied to CIFAR-10
+# Applied to CIFAR-100
+# Evaluated using FID Metric for Image Similarity
 
 """
-
-##
-import keras
-model = keras.models.load_model('cifar100_conditional_generator_75_epochs.h5')
 
 ##
 import numpy as np
@@ -35,8 +32,10 @@ from matplotlib import pyplot as plt
 
 plt.figure(figsize=(16,16))
 unique_labels = np.unique(y_train)
+print(unique_labels)
+
 for ii in unique_labels:
-    plt.subplot(2, 5, 1 + ii)
+    plt.subplot(4, 5, 1 + ii)
     first_instance_class_index = np.where(y_train == ii)
     plt.imshow(x_train[first_instance_class_index[0][0]])
     plt.title(str(ii))
@@ -45,10 +44,12 @@ for ii in unique_labels:
     ax.yaxis.set_tick_params(labelleft=False)
     ax.set_xticks([])
     ax.set_yticks([])
+    plt.savefig('CIFAR10.png')
 
-plt.savefig('CIFAR10.png')
 
+plt.tight_layout()
 plt.show()
+
 
 ## Data Loading and Simple Preprocessing Functions
 def load_CIFAR_Data():
@@ -75,7 +76,7 @@ def build_discriminator(input_image=(32,32,3)):
     in_label = keras.layers.Input(shape=(1,))
 
     # We will Map each Class n_classes to a different 50 element Vector Representation that will be learned by the Discriminator
-    layer_stack = keras.layers.Embedding(100, 50)(in_label)  # Embed 100 Class Representation into Vector of 50
+    layer_stack = keras.layers.Embedding(20, 50)(in_label)  # Embed 100 Class Representation into Vector of 50
 
 	# Scale up to the Image Size
     layer_stack = keras.layers.Dense(input_image[0] * input_image[1])(layer_stack)  # Learn Representation Image as Flattened Vector
@@ -125,7 +126,7 @@ def build_generator(latent_dim):
     in_label = keras.layers.Input(shape=(1,)) # Similar to Discriminator
 
 	# Each Label will be represented as a vector of 50
-    layer_stack = keras.layers.Embedding(100, 50)(in_label)  # Shape 100,50
+    layer_stack = keras.layers.Embedding(20, 50)(in_label)  # Shape 100,50
 
     # Linear Multiplication - > Since Input images are 32x32 and they are Downsampled Twice -> Need 8x8
     layer_stack = keras.layers.Dense(64)(layer_stack)  # 1,64
@@ -212,7 +213,7 @@ def generate_generator_inputs(latent_dim, n_samples):
     generator_input = latent_points.reshape(n_samples, latent_dim)
 
     # Generate Random Labels for the Inputs up to the True Number of Classes in Dataset
-    labels = np.random.randint(0, 100, n_samples)
+    labels = np.random.randint(0, 20, n_samples)
 
     return [generator_input, labels]
 
@@ -256,9 +257,9 @@ def generate_fake_samples(generator, latent_dim, n_samples):
 # 3.) Feed both sets into Discriminator
 # 4.) Set Loss for Real and Fake and Combined Loss
 
-r_loss = []
-f_loss = []
-model_loss = []
+g_losses = []
+d_losses = []
+
 
 def train(generator_model, discriminator_model, CGAN_Model, real_dataset, latent_dim, n_batch=128):
 
@@ -266,9 +267,8 @@ def train(generator_model, discriminator_model, CGAN_Model, real_dataset, latent
     n_batches_in_epoch = int(dataset[0].shape[0] / n_batch)
 
     # Epoch Loop
-    for i in range(75):
+    for i in range(200):
 
-        print('EPOCH: ' + str(i) )
         # Create a Dataset of 64 Fake and Real Images
         for j in range(n_batches_in_epoch):
 
@@ -277,15 +277,13 @@ def train(generator_model, discriminator_model, CGAN_Model, real_dataset, latent
 
             discriminator_real_loss , _ = discriminator_model.train_on_batch([X_Real, Labels_Real], Y_Real)
 
-            r_loss.append(discriminator_real_loss)
-
             # Generate Samples from the Generator
             [X_Fake, Fake_labels], Y_Fake = generate_fake_samples(generator_model, latent_dim, int(n_batch / 2))
 
             # Train Discriminator on Fake Data
             discriminator_fake_loss, _ = discriminator_model.train_on_batch([X_Fake, Fake_labels], Y_Fake)
 
-            f_loss.append(discriminator_fake_loss)
+            d_loss = 0.5 * np.add(discriminator_real_loss, discriminator_fake_loss) #Average loss if you want to report single..
 
             # Prepare Latent Point Vectors for Generator
             [generator_input, labels] = generate_generator_inputs(latent_dim, n_batch)
@@ -293,13 +291,13 @@ def train(generator_model, discriminator_model, CGAN_Model, real_dataset, latent
             # The Generator wants the Discriminator to Labels images as 1 or REAL
             Y_GAN = np.ones((n_batch, 1))
 
-            GAN_Loss = CGAN_Model.train_on_batch([generator_input, labels], Y_GAN)
+            g_loss = CGAN_Model.train_on_batch([generator_input, labels], Y_GAN)
 
-            model_loss.append(GAN_Loss)
+        g_losses.append(g_loss)
 
     # Save the Generator Model
-    generator_model.save('cifar100_conditional_generator_75_epochs.h5')
-    generator_model.save('./drive/MyDrive/ML_Project/cifar10_conditional_generator_75_epochs.h5')
+    generator_model.save('cifar10_conditional_generator_200_epochs.h5')
+    generator_model.save('./drive/MyDrive/ML_Project/cifar10_conditional_generator_200_epochs.h5')
 
 
 ## Train the CGAN
@@ -318,4 +316,150 @@ dataset = load_CIFAR_Data()
 train(generator_model, discriminator_model, CGAN_Model, dataset, latent_dim)
 
 
+     
 
+import tensorflow as tf
+
+model = tf.keras.saving.load_model('./cifar10_conditional_generator_200_epochs.h5')
+
+# generate multiple images
+
+latent_points, labels = generate_generator_inputs(100, 100)
+# specify labels - generate 10 sets of labels each gping from 0 to 9
+labels = np.asarray([x for _ in range(10) for x in range(10)])
+# generate images
+
+print(latent_points.shape)
+print(labels.shape)
+X  = model.predict([latent_points, labels])
+# scale from [-1,1] to [0,1]
+X = (X + 1) / 2.0
+X = (X*255).astype(np.uint8)
+# plot the result (10 sets of images, all images in a column should be of same class in the plot)
+# Plot generated images 
+
+plt.figure(figsize=(20,20))
+def show_plot(examples, n):
+	for i in range(n * n):
+		plt.subplot(n, n, 1 + i)
+		plt.axis('off')
+		plt.imshow(examples[i, :, :, :])
+		plt.title(labels[i])
+	
+	
+	plt.show()
+    
+show_plot(X, 10)
+plt.savefig('Samples_with_Conditioning_200_Epochs.png')
+
+## Plot the Discriminator and Generator Losses
+plt.figure(figsize=(12,8))
+plt.plot(g_losses,label='Generator Loss')
+plt.plot(d_losses,label='Discriminator Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.grid()
+plt.title('Discriminator vs. Generator Loss for CIFAR10 Training')
+plt.legend()
+plt.show()
+plt.savefig('Losses_Plot_200_Epochs.png')
+
+from skimage.transform import resize
+import scipy
+
+## Evaluate all the models and generated pictures vs. each Label using the Frechet Inception Distance (adapted from MLMastery)
+
+from keras.applications.inception_v3 import InceptionV3
+from keras.applications.inception_v3 import preprocess_input
+
+g_model = tf.keras.saving.load_model('./cifar10_conditional_generator_200_epochs.h5')
+
+model = InceptionV3(include_top=False, pooling='avg', input_shape=(299,299,3)) # We will have to Resize Images so they may be evaluated by InceptionV3
+
+(x_train, y_train), (x_test, y_test) = load_data() # Load the CIFAR10 Data
+
+# For the purpose of CGAN just checking FID is not enough, we want to do an evaluation per class to see how well the network was conditioned for labels. We will compare to the images in the training set.
+
+unique_labels = np.unique(y_train)
+
+
+# FID Calculator
+def calculate_fid(model, images1, images2):
+	# calculate activations
+	act1 = model.predict(images1)
+	act2 = model.predict(images2)
+	# calculate mean and covariance statistics
+	mu1, sigma1 = act1.mean(axis=0), np.cov(act1, rowvar=False)
+	mu2, sigma2 = act2.mean(axis=0), np.cov(act2, rowvar=False)
+	# calculate sum squared difference between means
+	ssdiff = np.sum((mu1 - mu2)**2.0)
+	# calculate sqrt of product between cov
+	covmean = scipy.linalg.sqrtm(sigma1.dot(sigma2))
+	# check and correct imaginary numbers from sqrt
+	if np.iscomplexobj(covmean):
+		covmean = covmean.real
+	# calculate score
+	fid = ssdiff + np.trace(sigma1 + sigma2 - 2.0 * covmean)
+	return fid
+
+# Image array scaling function
+def scale_images(images, new_shape):
+	images_list = list()
+	for image in images:
+		# resize with nearest neighbor interpolation
+		new_image = resize(image, new_shape, 0)
+		# store
+		images_list.append(new_image)
+	return np.asarray(images_list)
+
+
+FID_SCORES = np.zeros((3,len(unique_labels)))
+
+for jj in range(3):
+
+    if jj == 0:
+      g_model = tf.keras.saving.load_model('./cifar10_conditional_generator_mode_collapse.h5')
+      print('cGAN MODE COLLAPSE FID SCORES:')
+    if jj == 1:
+      g_model = tf.keras.saving.load_model('./cifar10_conditional_generator_75_epochs.h5')
+      print('cGAN 75 EPOCH TUNED FID SCORES:')
+    if jj == 2:
+      g_model = tf.keras.saving.load_model('./cifar10_conditional_generator_200_epochs.h5')
+      print('cGAN 200 EPOCH TUNED FID SCORES:')
+
+    for ii in unique_labels:
+        match_class_index = np.where(y_train == ii)
+        this_class_images = x_train[match_class_index[0]]
+        np.random.shuffle(this_class_images)
+        this_class_images = this_class_images[:500] # Only Compare 500 Real and 500 Fake
+        print(this_class_images.shape)
+
+        # Generate Fake Samples from same Class
+        latent_points, labels = generate_generator_inputs(100, len(this_class_images))
+        # Specify Labels
+        labels = np.ones(len(this_class_images)) * ii
+        # Generate Images
+        X  = g_model.predict([latent_points, labels])
+        # Scale Images to [0,1]
+        X = (X + 1) / 2.0
+        X = (X*255).astype(np.uint8)
+        # Convert to Floating Point
+        images_real = this_class_images.astype('float32')
+        images_fake = X.astype('float32')
+
+        print(X.shape)
+        # Resize Images
+        images_real = scale_images(images_real,(299,299,3))
+        images_fake = scale_images(images_fake,(299,299,3))
+
+        images_real = preprocess_input(images_real)  
+        images_fake = preprocess_input(images_fake)
+
+        # calculate fid
+        fid = calculate_fid(model, images_real, images_fake)
+        print('CLASS ' + str(ii))
+        print('FID: %.3f' % fid)
+        FID_SCORES[jj][ii] = fid
+
+
+print(FID_SCORES)
